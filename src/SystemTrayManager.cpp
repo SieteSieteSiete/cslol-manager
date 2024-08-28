2da30f1eaa306bf973ad4e0aa06bb0621e7ffe8b
SieteSieteSiete <67817758+SieteSieteSiete@users.noreply.github.com>
2024-08-28 16:57:56 -0600

    Forgot to include new files

---
+++ src/SystemTrayManager.cpp
@@ -0,0 +1,51 @@
+#include "SystemTrayManager.h"
+#include <QQmlContext>
+#include <QSystemTrayIcon>
+
+SystemTrayManager::SystemTrayManager(QObject *parent)
+    : QObject(parent), m_engine(nullptr), m_available(false)
+{
+}
+
+void SystemTrayManager::initialize(QQmlApplicationEngine *engine)
+{
+    m_engine = engine;
+    m_available = QSystemTrayIcon::isSystemTrayAvailable();
+    m_engine->rootContext()->setContextProperty("systemTrayManager", this);
+    emit availableChanged();
+}
+
+bool SystemTrayManager::isAvailable() const
+{
+    return m_available;
+}
+
+void SystemTrayManager::showWindow()
+{
+    emit windowVisibilityChanged(true);
+}
+
+void SystemTrayManager::hideWindow()
+{
+    emit windowVisibilityChanged(false);
+}
+
+void SystemTrayManager::runProfile()
+{
+    emit profileStateChanged(true);
+}
+
+void SystemTrayManager::stopProfile()
+{
+    emit profileStateChanged(false);
+}
+
+void SystemTrayManager::openLogs()
+{
+    // Implement log opening logic here
+}
+
+void SystemTrayManager::quit()
+{
+    QCoreApplication::quit();
+}
